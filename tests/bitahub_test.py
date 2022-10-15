b6ce6073c5e5e8c4bda10a05b282dd78fb0bcf9f
Wu Zhenyu <wuzy01@qq.com>
2022-10-15 21:16:50 +0800

    :art: Format test

--- tests/bitahub_test.py
+++ tests/bitahub_test.py
@@ -10,4 +10,9 @@ class Test:
         with open("assets/html/debug.html") as f:
             html = f.read()
         result = get_result(html)
-        assert result == "#[fg=black]8:#[fg=red]0 #[fg=black]7:#[fg=red]0 #[fg=black]6:#[fg=green]1 #[fg=black]5:#[fg=red]0 #[fg=black]4:#[fg=green]1 #[fg=black]3:#[fg=green]2 #[fg=black]2:#[fg=red]0 #[fg=black]1:#[fg=red]0#[fg=default]"  # noqa: E501
+        assert result == (
+            "#[fg=black]8:#[fg=red]0 #[fg=black]7:#[fg=red]0 "
+            "#[fg=black]6:#[fg=green]1 #[fg=black]5:#[fg=red]0 "
+            "#[fg=black]4:#[fg=green]1 #[fg=black]3:#[fg=green]2 "
+            "#[fg=black]2:#[fg=red]0 #[fg=black]1:#[fg=red]0#[fg=default]"
+        )
